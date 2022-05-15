3d411d146264cf372e3d43f0884e181495ccab45
celtic93 <egorka80@list.ru>
2022-05-15 14:01:51 +0300

    Added Arrays#replace method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_element = array[0]
+        array.each { |element| max_element = element if element > max_element }
+        array.map { |element| element.positive? ? max_element : element }
       end
 
       def search(_array, _query)
