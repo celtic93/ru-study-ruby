81a44b57bde7ff55100c3f8d19b913a95a124148
celtic93 <egorka80@list.ru>
2022-05-15 16:44:57 +0300

    Added Fp#rating method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,15 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        films_ratings = array.map do |film|
+          country_count = film['country'] ? film['country'].split(',').count : 0
+          is_raiting_positive = film['rating_kinopoisk']&.to_f&.positive?
+          film['rating_kinopoisk'].to_f if country_count >= 2 && is_raiting_positive
+        end
+
+        films_ratings.compact!
+        films_ratings.reduce(&:+) / films_ratings.count
       end
 
       def chars_count(_films, _threshold)
